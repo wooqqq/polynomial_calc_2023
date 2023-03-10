fea0f3ac145ad954f2f51532faa1d21bfb1bdbd1
깃wooqqq <sge0225@naver.com>
2023-03-10 16:48:13 +0900

    곱셈 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,31 +2,32 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
-        exp = exp.replaceAll("- ", "+ -");
+        boolean needToMulti = exp.contains("*");
+        boolean needToPlus = !needToMulti;
+
+        if (needToPlus) {
+            exp = exp.replaceAll("- ", "+ -");
 
 
-        String[] bits = exp.split(" \\+ ");
+            String[] bits = exp.split(" \\+ ");
 
+            int sum = 0;
 
-//        int a = Integer.parseInt(bits[0]);
-//        int b = Integer.parseInt(bits[1]);
-//        int c = 0;
-//        int d = 0;
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
 
-//        if (bits.length > 2) {
-//            c = Integer.parseInt(bits[2]);
-//        }
-//        if (bits.length > 3) {
-//            d = Integer.parseInt(bits[3]);
-//        }
-        int sum = 0;
+            return sum;
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+            int sum = 1;
 
-        for (int i = 0; i < bits.length; i++) {
-            sum += Integer.parseInt(bits[i]);
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
         }
-
-        return sum;
-
-//        throw new RuntimeException("올바른 계산식이 아닙니다.");
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 }
