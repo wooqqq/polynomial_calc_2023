1e6d5a348afd97eb538fd2851ead2e6dd9cdf158
깃wooqqq <sge0225@naver.com>
2023-03-10 16:43:45 +0900

    여연산 여러개

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -8,15 +8,24 @@ public class Calc {
         String[] bits = exp.split(" \\+ ");
 
 
-        int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[1]);
-        int c = 0;
+//        int a = Integer.parseInt(bits[0]);
+//        int b = Integer.parseInt(bits[1]);
+//        int c = 0;
+//        int d = 0;
 
-        if (bits.length > 2) {
-            c = Integer.parseInt(bits[2]);
+//        if (bits.length > 2) {
+//            c = Integer.parseInt(bits[2]);
+//        }
+//        if (bits.length > 3) {
+//            d = Integer.parseInt(bits[3]);
+//        }
+        int sum = 0;
+
+        for (int i = 0; i < bits.length; i++) {
+            sum += Integer.parseInt(bits[i]);
         }
 
-        return a + b + c;
+        return sum;
 
 //        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
--- src/test/java/com/ll/CalcTests.java
+++ src/test/java/com/ll/CalcTests.java
@@ -53,4 +53,16 @@ public class CalcTests {
     void t8() {
         assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
     }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 == -20")
+    void t9() {
+        assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+    void t10() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
 }
